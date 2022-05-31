fda796b6058f269a47dc0678123cdbab87f829c0
DrakeSterlingDeaton <41165256+DrakeSterlingDeaton@users.noreply.github.com>
2022-05-31 20:48:25 +0100

    fixes

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,29 @@
 
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
+  caches.open('static').then((a) => console.log(a));
+  event.waitUntil(
+    caches.open('static')
+    .then(function(cache) {
+      console.log('[Service Worker] Precaching App Shell');
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/src/js/app.js',
+        '/src/js/feed.js',
+        '/src/js/promise.js',
+        '/src/js/fetch.js',
+        'src/js/material.min.js',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/src/images/main-image.jpg',
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+      ])
+      caches.open('static').then((a) => console.log(a));
+    })
+  );
 });
 
 self.addEventListener('activate', function(event) {
@@ -9,19 +32,20 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-    
-    // for any fetch request, check the cache first to see if that request has been cached.
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                if (response) {
-                    // return the cached value (if it exists)
-                    return response;
-                } else {
-                    // otherwise, send the fetch request
-                    return fetch(event.request);
-                }
-            })
-            
-    );
+  // console.log('in fetch...');
+  // for any fetch request, check the cache first to see if that request has been cached.
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          console.log('interception!');
+          console.log(response);
+          // return the cached value (if it exists)
+          return response;
+        } else {
+          // otherwise, send the fetch request
+          return fetch(event.request);
+        }
+      })
+  );
 });
